551799200044fb1651919a5961d096cf2c7e916d
chencheng (yunqian) <sorrycc@gmail.com>
2011-06-15 23:35:08 +0800

    modify socket options

--- src/public/assets/socket.js
+++ src/public/assets/socket.js
@@ -11,7 +11,7 @@ var socket = new io.Socket(location.hostname, {
             reconnect: true,
             reconnectionDelay: 5,
             maxReconnectionAttempts: 1000000,
-            rememberTransport: false
+            rememberTransport: true
         });
 
 socket.base = {
