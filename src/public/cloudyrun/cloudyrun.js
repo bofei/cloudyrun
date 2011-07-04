20b4bd8fe4cb669218831ba31855055c50783e8e
chencheng (yunqian) <sorrycc@gmail.com>
2011-07-04 10:12:43 +0800

    bugfix

--- src/public/cloudyrun/cloudyrun.js
+++ src/public/cloudyrun/cloudyrun.js
@@ -88,7 +88,7 @@ var CloudyRun = {
                 var server = this._config.server;
                 this._post(server + 'post', data);
 
-                if (window.opener && url.indexOf('__newwindow__') > -1) {
+                if (window.opener && location.href.indexOf('__newwindow__') > -1) {
                     setTimeout(function() {
                         window.close();
                     }, 500);
