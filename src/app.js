0f9ab1898ef5fff060910f424a6c8cb914a16b48
chencheng (yunqian) <sorrycc@gmail.com>
2011-07-08 16:32:48 +0800

    bugfix

--- src/app.js
+++ src/app.js
@@ -112,7 +112,7 @@ app.post('/post', function(req, res) {
     res.end('');
 });
 
-var port = process.env.VMC_APP_PORT || process.env.PORT || 8080,
+var port = process.env.VMC_APP_PORT || process.env.PORT || 8080;
 app.listen(port, function() {
     log('started @ ' + port, 'info');
 });
